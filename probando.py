138e36ba4fe394d3e864770314d7840533103db4
launi145 <launi145@gmail.com>
2023-06-28 14:46:31 +0200

    le saque el print

---
+++ probando.py
@@ -0,0 +1,14 @@
+import csv
+
+# Abre el archivo CSV en modo lectura
+with open('datos.csv', 'r') as datos:
+    # Crea un objeto lector de CSV
+    lector_csv = csv.reader(datos)  # método específico para csv
+    encabezado = next(lector_csv)
+    print(encabezado)
+    print(".....")
+        
+
+    for fila in lector_csv:
+      print(fila)
+      for celda in fila:
